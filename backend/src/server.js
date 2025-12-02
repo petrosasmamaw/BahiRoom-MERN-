3b9c6d1133cf094ec8602bbef88300ccdc283361
Petros Asmamaw <petrosasmamaw2@gmail.com>
2025-12-02 21:36:42 +0300

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json()); 
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {console.log("✅ MongoDB Connected Successfully!"); })
+    .catch((err) => {console.error("Error connecting to MongoDB:", err);});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
